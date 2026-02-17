9c943fc367c210b13f4b8d718af4899340f406c6
Darius van Niekerk <u22547216@tuks.co.za>
2026-02-17 08:34:52 +0200

    Creating the main code file 	Added Port setups 		PortA 		PortC 		PortD 	Added sections for parts of the code 		unit tests 		sensor reading 		calibration sequence 		line location interpreter 		Delay for the calibration flashes

---
+++ MARV_code.s
@@ -0,0 +1,90 @@
+; -----------------------------------------------------------------------------
+; EMK310
+; MARV CODE
+; TEAM: 28
+; MEMBERS: Darius van Niekerk
+; Date of last revision: February 2026
+;------------------------------------------------------------------------------
+; Description: 
+;   Code for the MARV of team 28 for EMK 310 in 2026
+; -----------------------------------------------------------------------------
+; Details:
+;	ADC:
+;	    Pins:   RA0,1,2,3,5
+;	    ADCON0: AN0,1,2,3,4 (binary: 0 00000 00 to 0 00101 00)
+;	Register dump:
+;	    Port C
+;	Colour display:
+;	    Port D
+;		
+; -----------------------------------------------------------------------------
+
+    title	"Our second assembler program"
+    PROCESSOR	18F45K22
+    
+    ; CONFIG1H
+    CONFIG  FOSC = INTIO67        ; Oscillator Selection bits (Internal oscillator block)
+				  ; There is a how-to tutorial on the configuration bits
+  
+    
+    #include    <xc.inc>
+    #include    "pic18f45k22.inc"
+;
+; -------------	
+; PROGRAM START	
+; -------------
+;
+    PSECT code,abs //Start of main code.
+    org	    init 			; startup address = 0000h
+
+init:
+    MOVLB   0xF		; work in bank 15, not all SFRs are in access bank
+    
+    ; setup ADC pins
+    CLRF    PORTA,a 	; Initialize PORTA by clearing output data latches
+    CLRF    LATA,a 	; Alternate method to clear output data latches
+    movlw   0b00101111
+    movwf   ANSELA,b 	; set pins A 0,1,2,3 and 5 to analogue
+    movwf   TRISA,a	; set pins A 0,1,2,3 and 5 to input
+    
+    ; setup debug ports(C and D)
+    ; register dump port
+    clrf    PORTC, a
+    clrf    LATC, a
+    clrf    ANSELC, b
+    clrf    TRISC, a
+    
+    ; colour show port
+    clrf    PORTD, a
+    clrf    LATD, a
+    clrf    ANSELD, b
+    clrf    TRISD, a
+    
+    MOVLB   0x00	; back to bank 0 for normal opperations
+		
+		
+start: 	
+    movlw 		0x0F 		; move 0FH to W register
+    movwf 		0F80h,a		; move W to PORTA
+    movlw 		0x00 		; move 00H to W register
+    movwf 		PORTA,a		; move W to PORTA - note use of PORTA vs address
+    goto 		start 		; do this loop forever
+	
+register_dump:
+    
+show_colour:
+    
+read_sensors:
+    
+calibration:
+    
+LLI:
+    
+delay_333:
+    
+    end			
+
+
+
+
+
